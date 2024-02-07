fe9c2831b0bc4c87be37f2552644119f3bd2ef3b
xepoman <ivan.egov@mail.ru>
2024-02-07 12:52:33 +0700

    Добавил проект

---
+++ Katena/Program.cs
@@ -0,0 +1,37 @@
+/*using Katena.Domain;
+using Katena.Domain.Repositories.Abstruct;
+using Katena.Domain.Repositories.EntietyFramework;*/
+using Katena.Service;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+var app = builder.Build();
+
+IConfiguration configuration = app.Configuration;
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ Katena/Service/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Katena.Service
+{
+	public class Сonfig
+	{
+        public static string ConnectionString { get; set; }
+        public static string Company { get; set; }
+        public static string CompanyEmail { get; set; }
+    }
+}
