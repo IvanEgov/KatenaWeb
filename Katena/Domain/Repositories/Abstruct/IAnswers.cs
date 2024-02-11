0bb2470e35337984771e5912bee9ba9d2621fbbb
FuSseNet <kolya190509@mail.ru>
2024-02-12 00:33:51 +0700

    Update IAnswers.cs

--- Katena/Domain/Repositories/Abstruct/IAnswers.cs
+++ Katena/Domain/Repositories/Abstruct/IAnswers.cs
@@ -2,6 +2,8 @@
 
 namespace Katena.Domain.Repositories.Abstruct
 {
+    //Публичный интерфейс, позволяющий получать все ответы, получать ответ по id, сохранять новый ответ или изменять уже существующий
+    //Удалять ответ по id
     public interface IAnswers
     {
         IQueryable<AnswersBase> GetAllAnswers();
