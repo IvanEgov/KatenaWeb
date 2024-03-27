4a33c28d37d3131c32ed749fd09b0c5399677cab
FuSseNet <kolya190509@mail.ru>
2024-03-27 16:51:10 +0700

    TestViewComponent
    Done First version of ViewComponent for test page

--- Katena/Components/TestComponents/TestViewComponent.cs
+++ Katena/Components/TestComponents/TestViewComponent.cs
@@ -1,6 +1,30 @@
-﻿namespace Katena.Components
+﻿using Katena.Areas.Admin.Controllers;
+using Katena.Domain;
+using Katena.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Katena.Components
 {
-    public class TestViewComponent
+    
+    public class Test : ViewComponent
     {
+        private readonly DataManager _dataManager;
+
+        public IViewComponentResult Invoke(QuestionPackBase pack, int index = -1)
+        {
+            if (index == -1)
+            {
+                return View("TestWelcome", pack);
+            }
+            else if (index < pack.QuestionsIds.Count)
+            {
+                QuestionBase question = _dataManager.QuestionBase.GetQuestionById(pack.QuestionsIds[index]);
+                var model = new { question, index };
+                return View("TestProcess", model);
+            }
+            return View("TestFinish", pack);
+        }
+
     }
 }
