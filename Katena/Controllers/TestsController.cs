a83874426e911dfd80fe6c55f8a9e3a61aaed198
FuSseNet <kolya190509@mail.ru>
2024-04-01 23:31:51 +0700

    Continue edit view

--- Katena/Controllers/TestsController.cs
+++ Katena/Controllers/TestsController.cs
@@ -52,18 +52,5 @@ namespace Katena.Controllers
 			ViewBag.index = index;
 			return View("Index", dataManager.TextFields.GetTextFieldByCodeWord("PageTests"));
 		}
-
-		//We need this method to get ViewComponent to the reason part (I think, it should work)
-		/*[HttpPost]
-		public IActionResult SwitchToResason(Guid pack, int index, Guid answers)
-		{
-			return ViewComponent("Test", new
-			{
-				pack = dataManager.Packs.GetPackById(pack),
-				index = index,
-				answer = dataManager.Answers.GetAnswerById(answers),
-				reason = true
-			});
-		}*/
 	}
 }
